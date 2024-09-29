096613d69909033b517fe0e022bc34b408a09f05
ninjarlz <michal.kusmidrowicz@gmail.com>
2024-09-29 18:01:56 +0200

    scopes removal from user details dto

--- src/main/java/pl/tul/smartbot/model/dto/security/UserDetailsDTO.java
+++ src/main/java/pl/tul/smartbot/model/dto/security/UserDetailsDTO.java
@@ -3,11 +3,9 @@ package pl.tul.smartbot.model.dto.security;
 import jakarta.annotation.Nullable;
 import lombok.Builder;
 
-import java.util.Set;
-
 /**
  * Object allows storing user details obtained from JWT.
  */
 @Builder
-public record UserDetailsDTO(long userId, @Nullable String username, @Nullable String email, Set<String> scopes) {
+public record UserDetailsDTO(long userId, @Nullable String username, @Nullable String email) {
 }
