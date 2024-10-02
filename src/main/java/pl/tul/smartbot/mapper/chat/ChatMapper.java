64d9951c234b635c6ec68d9213924d7baf3fd310
ninjarlz <michal.kusmidrowicz@gmail.com>
2024-10-02 13:56:06 +0200

    mappers component model

--- src/main/java/pl/tul/smartbot/mapper/chat/ChatMapper.java
+++ src/main/java/pl/tul/smartbot/mapper/chat/ChatMapper.java
@@ -1,10 +1,11 @@
 package pl.tul.smartbot.mapper.chat;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import pl.tul.smartbot.model.dto.chat.ChatDTO;
 import pl.tul.smartbot.model.entity.chat.ChatEntity;
 
-@Mapper
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ChatMapper {
     ChatEntity DTOtoEntity(ChatDTO chatDTO);
     ChatDTO entityToDTO(ChatEntity chatEntity);
--- src/main/java/pl/tul/smartbot/mapper/message/MessageMapper.java
+++ src/main/java/pl/tul/smartbot/mapper/message/MessageMapper.java
@@ -1,13 +1,14 @@
 package pl.tul.smartbot.mapper.message;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import pl.tul.smartbot.model.dto.message.MessageDTO;
 import pl.tul.smartbot.model.entity.message.AuthorType;
 import pl.tul.smartbot.model.entity.message.MessageEntity;
 import pl.tul.smartbot.model.request.message.MessageRequestV1;
 import pl.tul.smartbot.model.response.message.MessageResponseV1;
 
-@Mapper
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public abstract class MessageMapper {
 
     public MessageEntity DTOtoEntity(AuthorType authorType, MessageDTO messageDTO) {
